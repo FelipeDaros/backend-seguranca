d905af3dee8a3286719cff8f3c9a2fc1958295d9
FelipeDaros <felipe-daros@hotmail.com>
2022-08-04 09:50:15 -0300

    .

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,9 @@
 import { PartialType } from "@nestjs/mapped-types";
+import { IsBoolean } from "class-validator";
 import { CreateUserDto } from "./create-user.dto";
 
 
-export class UpdateUserDto extends PartialType(CreateUserDto){}
+export class UpdateUserDto extends PartialType(CreateUserDto){
+  @IsBoolean()
+  isAdmin: boolean;
+}
